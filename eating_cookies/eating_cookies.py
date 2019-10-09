85b0554d6a48ab0f64652bb566dcb34059623e44
GianBarGian <giacomobenati@hotmail.it>
2019-10-09 16:06:24 +0200

    second pass on eating cookies done

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,21 +5,26 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-  
 def eating_cookies(n, cache=None):
-  result = 0
-  
+  if cache and cache[n] != 0:
+    return cache[n]
+  if not cache:
+    cache = [0 for i in range(n + 1)]
   if n == 0:
-    return 1
-  if n > 0:
-    result += eating_cookies(n - 1)
-  if n > 1:
-    result += eating_cookies(n - 2)
-  if n > 2:
-    result += eating_cookies(n - 3)
-  return result
+    value = 1
+  if n == 3:
+    value = 4
+  if n == 2:
+    value = 2
+  if n == 1:
+    value = 1
+  if n > 3:
+    value = eating_cookies(n - 3, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 1, cache)
+  
+  cache[n] = value
+  return value
 
-print(eating_cookies(3, 0))
+print(eating_cookies(50))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
